91ad63736a08ca3c0f4dd9a6b8225d489f391154
valeryO <olennval@mail.ru>
2022-08-05 23:28:38 +0700

    reshenie # 8

---
+++ numer8/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("введите натуральное число > 1");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 2; i <= n; i += 2)
+{
+    Console.Write(i);
+    Console.Write(" ");
+}
