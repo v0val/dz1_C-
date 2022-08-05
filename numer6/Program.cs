69332df96be31a6e9b3ee18fdd094f060c6930af
valeryO <olennval@mail.ru>
2022-08-05 23:15:17 +0700

    reshenie # 6

---
+++ numer6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("введите целое число");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.Write(a);
+    Console.WriteLine(" - чётное" );
+}
+else
+{
+    Console.Write(a);
+    Console.WriteLine(" - не является чётным" );
+}
+   
