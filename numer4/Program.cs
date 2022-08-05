38d34ad0ab791182b0e13311ce4f39a6932e6716
valeryO <olennval@mail.ru>
2022-08-05 23:07:01 +0700

    reshenie # 4

--- numer4/Program.cs
+++ numer4/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("введите три целых числа");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int maxim = a;
+if (b > maxim)
+{
+    maxim = b;
+}
+if (c > maxim)
+{
+    maxim = c;
+}
+    Console.Write(maxim);
+    Console.WriteLine(" - наибольшее" );
