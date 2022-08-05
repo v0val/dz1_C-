16d45c4ae4db270bd3df1bd848a4b5b05fb78053
valeryO <olennval@mail.ru>
2022-08-05 22:55:47 +0700

    number 2

---
+++ number1/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("введите два целых числа");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.Write(a);
+    Console.WriteLine(" - наибольшее" );
+    Console.Write(b);
+    Console.WriteLine(" - наименьшее" );
+}
+if (a < b)
+{
+    Console.Write(b);
+    Console.WriteLine(" - наибольшее" );
+    Console.Write(a);
+    Console.WriteLine(" - наименьшее" );
+}
+if (a == b)
+{
+    Console.Write(a);
+    Console.Write(" = " );
+    Console.Write(b);
+}
